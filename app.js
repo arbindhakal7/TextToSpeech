a8796992fac28a88099f501644b0627c7280049b
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-26 12:03:59 +0545

    :construction: work on progress

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const textarea = document.querySelector("textarea"),
 speechBtn = document.querySelector("button");
+
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        textToSpeech(textarea.value);
+    }
+});
