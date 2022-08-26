c059d2ee8cc62064abe0c2a27f854ef7a202b1f9
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-26 12:12:22 +0545

    :zap: voices logic

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const textarea = document.querySelector("textarea"),
-speechBtn = document.querySelector("button")
+speechBtn = document.querySelector("button"),
+voiceList = document.querySelector("select")
 
+let synth = speechSynthesis
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
 
 function textToSpeech(text){
     let utternance = new SpeechSynthesisUtterance(text)
