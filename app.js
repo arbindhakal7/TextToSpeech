a9c239bf1e824206313482817b650aaaad4923fa
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-26 12:21:12 +0545

    :bulb: comments added for voices

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ let synth = speechSynthesis
 
 function voices(){
     for(let voice of synth.getVoices()){
+        //select Google US English voice as default
         let selected = voice.name === "Google US English" ? "selected" : "";
+        // creating an option tag passing voice name and language
         let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
-        voiceList.insertAdjacentHTML("beforeend", option);
+        voiceList.insertAdjacentHTML("beforeend", option); //insert option tag before end of select tag
     }
 }
 
+synth.addEventListener("voiceschanged", voices);
+
 function textToSpeech(text){
     let utternance = new SpeechSynthesisUtterance(text)
+
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utterance.voice = voice;
+        }
+    }
+    
     speechSynthesis.speak(utternance)
 }
 
