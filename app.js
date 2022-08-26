e438f61859d8a6036799a3ca65b8eb558f6b0de8
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-26 12:05:59 +0545

    :zap: speak the speech/utternance

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const textarea = document.querySelector("textarea"),
-speechBtn = document.querySelector("button");
+speechBtn = document.querySelector("button")
 
+
+function textToSpeech(text){
+    let utternance = new SpeechSynthesisUtterance(text)
+    speechSynthesis.speak(utternance)
+}
 
 speechBtn.addEventListener("click", e =>{
     e.preventDefault();
     if(textarea.value !== ""){
-        textToSpeech(textarea.value);
+        textToSpeech(textarea.value)
     }
 });
